b87d8e2e2df8303d9d8dd1a1f839b6609c89b4ab
Mark Manning <mmanning@isecpartners.com>
2014-12-22 15:06:13 -0500

    adding agenda support. Reworked to be able to find the titles in existing slides

--- remark.py
+++ remark.py
@@ -10,36 +10,56 @@ class Remark:
     def __init__(self, template=None):
         ## Create new remark instance
         self.slides = []
+        self.md = []
 
-    def add_slide(self, slide):
+    def build_slides(self):
         """ Receive a dict containing title, highlights(list), and url
         slide["title"], slide["url"], slide["higlights"] """
-        content = []
-        #content.append("class: center, middle") ##TODO change to dynamic
-        content.append("## " + slide["title"])
-        highlights = []
-        for h in slide["highlights"]:
-            content.append("- " + h)
-        content.append(slide["url"])
-        content.append("---")
-        self.slides.append(content)
+        md = ""
+        for slide in reversed(self.slides):
+            content = []
+            #content.append("class: center, middle") ##TODO change to dynamic
+            content.append("## " + slide["title"])
+            highlights = []
+            for h in slide["highlights"]:
+                content.append("- " + h)
+            content.append(slide["url"])
+            content.append("---")
+
+            for line in content:
+              md += line + "\n\n"
+        return md
+
+    def add_slide(self, slide):
+        self.slides.append(slide)
 
     def _test_slide(self):
         slide = {}
         slide["title"] = "Test title"
-        slide["url"] = "Http://www.google.com"
-        highlights = ["Highlight 1", "more", "Something else"]
+        slide["url"] = "http://www.google.com"
+        highlights = [
+            "Highlight item #1",
+            "this is a long highlight about something or other. It's not very interesting",
+            "Something else"
+        ]
         slide["highlights"] = highlights
         return slide
 
+    def add_agenda(self):
+        slide = {}
+        slide["title"] = "Agenda"
+        titles = []
+        for s in self.slides:
+            titles.append(s["title"])
+        slide["highlights"] = titles
+        slide["url"] = ""
+        self.add_slide(slide)
+
     def build(self):
         """ Returns just the MD of the slides"""
-        md = ""
-        for slide in self.slides:
-            for line in slide:
-                md += line + "\n\n"
+        self.add_agenda()
+        md = self.build_slides()
         return md
-
 
 if __name__ == '__main__':
     print("")
